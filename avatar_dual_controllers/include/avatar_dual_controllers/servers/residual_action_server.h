92cb71e55aa05d240d81c64f5b7379201afaa762
daegyulim <dgyo3784@gmail.com>
2021-11-25 23:40:24 +0900

    Create random collision free trajectory

--- avatar_dual_controllers/include/avatar_dual_controllers/servers/residual_action_server.h
+++ avatar_dual_controllers/include/avatar_dual_controllers/servers/residual_action_server.h
@@ -4,6 +4,14 @@
 #include <avatar_dual_controllers/utils/dyros_math.h>
 #include <avatar_msgs/SetTrajectoryFollowerGain.h>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_msgs/CollisionObject.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+#include <random>
+
 #define RobotName "panda_right"
 
 using namespace dyros_math;
@@ -25,6 +33,8 @@ public:
   Eigen::Vector7d q_desired_;
   Eigen::Vector7d qd_desired_;
 
+  int num_dof_ = 7;
+
   ResidualActionServer(std::string name, ros::NodeHandle &nh, 
                           std::map<std::string, std::shared_ptr<FrankaModelUpdater> > &mu);
                           
@@ -33,6 +43,10 @@ public:
 
   bool setTarget(ros::Time time);
 
+  void initMoveit();
+  void setMoveitObstables();
+  void generateRandTraj();
+
   bool first_compute_=true;
 
   double init_time_;
@@ -40,6 +54,30 @@ public:
   Eigen::Vector7d init_qd_;
 
   ros::ServiceServer server_ ;
+
+  // Moveit
+  inline static const std::string PLANNING_GROUP="panda_arm";
+  moveit::planning_interface::MoveGroupInterface move_group_;
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+  moveit::planning_interface::MoveGroupInterface::Plan random_plan_;
+  moveit::planning_interface::MoveGroupInterface::Plan random_plan_next_;
+
+  std::vector<double> q_target_plan_;
+  std::vector<double> q_target_plan_candidate_;
+  std::vector<double> q_init_plan_; 
+  std::vector<double> q_dot_plan_;
+
+  Eigen::VectorXd q_limit_u_;
+  Eigen::VectorXd q_limit_l_;
+
+  double traj_init_time_ = 0.0;
+  double traj_duration_ = 0.0;
+  int total_waypoints_;
+  int cur_waypoint_ = 0;
+  int traj_num_ = 0;
+
+  bool init_traj_prepared_ = false;
+  bool next_traj_prepared_ = false;
 
 private:
   bool setGain(avatar_msgs::SetTrajectoryFollowerGain::Request  &req,
--- avatar_dual_controllers/src/servers/residual_action_server.cpp
+++ avatar_dual_controllers/src/servers/residual_action_server.cpp
@@ -2,7 +2,7 @@
 
 ResidualActionServer::ResidualActionServer(std::string name, ros::NodeHandle &nh, 
                                 std::map<std::string, std::shared_ptr<FrankaModelUpdater> > &mu)
-: ActionServerBase(name,nh,mu)
+: ActionServerBase(name,nh,mu), move_group_(PLANNING_GROUP)
 {
   std::string joint_name_prefix = "_joint";
   for (auto iter = mu_.begin(); iter != mu_.end(); iter++)
@@ -27,6 +27,11 @@ ResidualActionServer::ResidualActionServer(std::string name, ros::NodeHandle &nh
 
     if (iter->first == RobotName)
       control_running_ = true;
+
+    // Moveit
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    initMoveit();
   }
 
   server_  = nh_.advertiseService(name + "_gain_set", &ResidualActionServer::setGain, this);
@@ -41,6 +46,155 @@ void ResidualActionServer::preemptCallback()
   
 }
 
+void ResidualActionServer::initMoveit()
+{
+    // For Moveit
+    q_limit_l_.resize(num_dof_);
+    q_limit_u_.resize(num_dof_);
+    q_limit_l_ << -2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0873, -2.8973;
+    q_limit_u_ <<  2.8973,  1.7628,  2.8973,  0.0698,  2.8973,  2.1127,  2.8973;
+
+    q_target_plan_.resize(num_dof_);
+    q_target_plan_candidate_.resize(num_dof_);
+    q_init_plan_.resize(num_dof_);
+    q_dot_plan_.resize(num_dof_);
+    // std::fill(q_target_plan_.begin(), q_target_plan_.end(), 0);
+    q_target_plan_[0] = 0.0;
+    q_target_plan_[1] = 0.0;
+    q_target_plan_[2] = 0.0;
+    q_target_plan_[3] = -90*DEG2RAD;
+    q_target_plan_[4] = 0.0;
+    q_target_plan_[5] = 90*DEG2RAD;
+    q_target_plan_[6] = 0.0;
+    std::fill(q_init_plan_.begin(), q_init_plan_.end(), 0);
+    std::fill(q_dot_plan_.begin(), q_dot_plan_.end(), 0);
+
+    const robot_state::JointModelGroup* joint_model_group = move_group_.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+    move_group_.setMaxVelocityScalingFactor(1.0);
+
+
+    setMoveitObstables();
+}
+
+void ResidualActionServer::setMoveitObstables()
+{
+    // Create collision object
+    std::vector<moveit_msgs::CollisionObject> collision_objects;
+    
+    shape_msgs::SolidPrimitive primitive;
+    primitive.type = primitive.BOX;
+    primitive.dimensions.resize(3);
+
+    double square_len = 4.0;
+    double hole_len = 0.22;
+    double y_offset = 0.0;
+    double height_offset = 0.0;
+    // Box 1
+    primitive.dimensions[0] = (square_len - hole_len)/2.0;
+    primitive.dimensions[1] = square_len;
+    primitive.dimensions[2] = 0.01;
+    moveit_msgs::CollisionObject box1;
+    box1.header.frame_id = move_group_.getPlanningFrame();
+    box1.id = "box1";
+    geometry_msgs::Pose box1_pose;
+    box1_pose.position.x = (hole_len + primitive.dimensions[0])/2.0;
+    box1_pose.position.y = y_offset;
+    box1_pose.position.z = height_offset + 0.095;
+    box1.primitives.push_back(primitive);
+    box1.primitive_poses.push_back(box1_pose);
+    box1.operation = box1.ADD;
+    collision_objects.push_back(box1);
+
+    // Box 2
+    moveit_msgs::CollisionObject box2;
+    box2.header.frame_id = move_group_.getPlanningFrame();
+    box2.id = "box2";
+    geometry_msgs::Pose box2_pose;
+    box2_pose.position.x = -(hole_len + primitive.dimensions[0])/2.0;
+    box2_pose.position.y = y_offset;
+    box2_pose.position.z = height_offset + 0.095;
+    box2.primitives.push_back(primitive);
+    box2.primitive_poses.push_back(box2_pose);
+    box2.operation = box2.ADD;
+    collision_objects.push_back(box2);
+
+    // Box 3
+    primitive.dimensions[0] = hole_len;
+    primitive.dimensions[1] = (square_len - hole_len)/2.0;;
+    moveit_msgs::CollisionObject box3;
+    box3.header.frame_id = move_group_.getPlanningFrame();
+    box3.id = "box3";
+    geometry_msgs::Pose box3_pose;
+    box3_pose.position.x = 0.0;
+    box3_pose.position.y = y_offset + (primitive.dimensions[1] + hole_len) / 2.0;
+    box3_pose.position.z = height_offset + 0.095;
+    box3.primitives.push_back(primitive);
+    box3.primitive_poses.push_back(box3_pose);
+    box3.operation = box3.ADD;
+    collision_objects.push_back(box3);
+
+    // Box 4
+    moveit_msgs::CollisionObject box4;
+    box4.header.frame_id = move_group_.getPlanningFrame();
+    box4.id = "box4";
+    geometry_msgs::Pose box4_pose;
+    box4_pose.position.x = 0.0;
+    box4_pose.position.y = y_offset + -(primitive.dimensions[1] + hole_len) / 2.0;;
+    box4_pose.position.z = height_offset + 0.095;
+    box4.primitives.push_back(primitive);
+    box4.primitive_poses.push_back(box4_pose);
+    box4.operation = box4.ADD;
+    collision_objects.push_back(box4);
+
+    planning_scene_interface_.addCollisionObjects(collision_objects);
+}
+
+void ResidualActionServer::generateRandTraj()
+{
+    if (!next_traj_prepared_)
+    {
+        moveit::core::RobotState start_state = *(move_group_.getCurrentState());
+        for (int i = 0; i < num_dof_; i++)
+        {
+            q_init_plan_[i] = q_target_plan_[i];
+            q_dot_plan_[i] = 0.0;
+        }
+
+        start_state.setJointGroupPositions(PLANNING_GROUP, q_init_plan_);
+        start_state.setJointGroupVelocities(PLANNING_GROUP, q_dot_plan_);
+        move_group_.setStartState(start_state);
+
+        std::random_device rand_device;
+        std::default_random_engine rand_seed;
+        std::uniform_real_distribution<double> angles[num_dof_];
+        rand_seed.seed(rand_device());
+        
+        double safe_range = 0.0;
+        for (size_t i = 0; i < num_dof_; i++)
+        {
+            safe_range = ((q_limit_u_[i] - q_limit_l_[i]) * 0.1);
+            angles[i] = std::uniform_real_distribution<double>((q_limit_l_[i] + safe_range), (q_limit_u_[i] - safe_range));
+        }
+        
+        for (int i = 0; i < num_dof_; i++) 
+            q_target_plan_candidate_[i] = ((angles[i])(rand_seed));
+  
+        move_group_.setJointValueTarget(q_target_plan_candidate_);
+
+        if (move_group_.plan(random_plan_next_) == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+        {
+          q_target_plan_ = q_target_plan_candidate_;
+          next_traj_prepared_ = true;
+
+          if (traj_num_ == 0)
+            init_traj_prepared_ = true;
+
+          traj_num_++;
+          std::cout<< "Trajectory " << traj_num_ << " prepared"<<std::endl;
+        }
+    }
+}
 
 bool ResidualActionServer::compute(ros::Time time)
 {
@@ -59,6 +213,7 @@ bool ResidualActionServer::compute(ros::Time time)
         init_qd_ = mu_[arm.first]->qd_;
         first_compute_ = false;
       }
+      generateRandTraj();
       setTarget(time);    
       
       if (arm.second == true)
